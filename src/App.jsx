e5b54f8198faddf56062dc05bbb1c3d528d5222b
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-04-12 15:44:34 -0700

    changed tailwind config added componenents,constatns,public,utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <h1>
-    "HELLO WORLD"
-    </h1>
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   )
 }
 
