a5ce1f7292d8b990f35d5ae0cba8997d0a935529
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-04-17 09:49:28 -0700

    finished features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./components/Features"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
@@ -5,15 +6,15 @@ import Navbar from "./components/Navbar"
 import * as Sentry from '@sentry/react'
 
 const App = () => {
-  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
-  // return (
-  //   <main className='bg-black'>
-  //     <Navbar />
-  //     <Hero />
-  //     <Highlights />
-  //     <Model />
-  //   </main>
-  // )
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+    </main>
+  )
 }
 
 export default Sentry.withProfiler(App);
